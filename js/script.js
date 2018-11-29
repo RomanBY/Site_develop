2996062834e9b153cdb521037e42462b21b4c94f
RomanBY <vasikroman@mail.ru>
2018-11-29 23:10:22 +0300

    add some blocks and scroll animate js

---
+++ js/script.js
@@ -0,0 +1,59 @@
+/*skills scroll*/
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 600) {
+        $('.line__progress-1').addClass('line__progress-w1');
+        $('.line__progress-2').addClass('line__progress-w2');
+        $('.line__progress-3').addClass('line__progress-w3');
+
+        $('.skills_prt-1').animate({ num: 75 - 3 }, {
+            duration: 3100,
+            step: function (num){
+                this.innerHTML = (num + 3).toFixed(0)+'%'
+            }
+        });
+        $('.skills_prt-2').animate({ num: 90 - 3 }, {
+            duration: 3100,
+            step: function (num){
+                this.innerHTML = (num + 3).toFixed(0)+'%'
+            }
+        });
+        $('.skills_prt-3').animate({ num: 65 - 3 }, {
+            duration: 3100,
+            step: function (num){
+                this.innerHTML = (num + 3).toFixed(0)+'%'
+            }
+        });
+    }
+});
+
+/*statistics numbers*/
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 1000){
+        $('.static-info-1').animate({num:548 - 3}, {
+            duration:3500,
+            step: function (num) {
+                this.innerHTML= (num + 3).toFixed(0)
+            }
+        });
+        $('.static-info-2').animate({num:1465 - 3}, {
+            duration:3500,
+            step: function (num) {
+                this.innerHTML= (num + 3).toFixed(0)
+            }
+        });
+        $('.static-info-3').animate({num:612 - 3}, {
+            duration:3500,
+            step: function (num) {
+                this.innerHTML= (num + 3).toFixed(0)
+            }
+        });
+        $('.static-info-4').animate({num:735 - 3}, {
+            duration:3500,
+            step: function (num) {
+                this.innerHTML= (num + 3).toFixed(0)
+            }
+        });
+    }
+
+})
+
